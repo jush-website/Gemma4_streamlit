444e60ef1a0a8e9f6ece8c26a9ff4bb3b3753c71
Jush <102575403+jush-website@users.noreply.github.com>
2026-04-12 00:57:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tavily import TavilyClient
 # ================= 設定區 =================
 OLLAMA_API_BASE = "https://bonding-malt-nimbly.ngrok-free.dev"
 MODEL_NAME = "ollama/gemma4:e4b" 
-TAVILY_API_KEY = "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"  # 👈 記得貼上你的專屬金鑰！
+TAVILY_API_KEY = "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"  
 # ==========================================
 
 # ---------------------------------------------------------
